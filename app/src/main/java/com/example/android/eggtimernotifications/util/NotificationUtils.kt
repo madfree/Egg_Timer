318260ef736a51bdc962d4da77581314bb4684ca
madfree <matzefrei@gmail.com>
2020-09-08 21:44:50 +0200

    feat: Cancel notification if new timer is started

--- app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/util/NotificationUtils.kt
@@ -82,3 +82,10 @@ fun NotificationManager.sendNotification(messageBody: String, applicationContext
 }
 
 // TODO: Step 1.14 Cancel all notifications
+/**
+ * Cancels all notifications.
+ *
+ */
+fun NotificationManager.cancelNotifications() {
+    cancelAll()
+}
